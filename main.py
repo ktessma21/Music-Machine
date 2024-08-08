c5f051a439da840d94ccabd15604b3ae2e93d2e5
ktessma21 <142500915+ktessma21@users.noreply.github.com>
2024-08-08 18:16:09 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,45 @@
+from musics import MUSICS
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+import json
+
+# writeJson
+
+def writeJson(jsonFile):
+    with open("myjson1.json", "w") as f:
+        json.dump(jsonFile, f, indent = 4)
+
+##### Spotify Playlist Building
+# authentitcating the spotify through the library. 
+
+
+SCOPE = "playlist-modify-private"
+USERNAME = "HelloKidusME"
+CLIENT_ID = "7cd30832615c4a17967778d1849cc75c"
+CLIENT_SECRET = "abe76dc280d343bb9e4784b4e1b8871e"
+REDIRECTING_URL = "https://github.com/ktessma21"
+
+scope = "playlist-modify-public"
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID, client_secret=CLIENT_SECRET,redirect_uri=REDIRECTING_URL,scope=scope))
+
+userID = sp.me()["id"]
+
+# create a playlist
+# sp.user_playlist_create(userID, "top 100 musics in 2024-05-12")
+# DONE
+
+# find the playlist ID
+playlistID = sp.current_user_playlists()["items"][0]["id"]
+
+# find the URIs/URLs of each music 
+
+trackURIS = []
+for music in MUSICS:
+    track  = sp.search(q=music)
+    trackURI = track["tracks"]["items"][0]["uri"]
+    trackURIS.append(trackURI)
+
+# writeJson(trackURI)
+
+sp.playlist_add_items(playlist_id=playlistID, items=trackURIS, position=None)
+
---
+++ musics.py
@@ -0,0 +1,22 @@
+import requests
+from bs4 import BeautifulSoup   
+
+
+
+# web-scrapping the billboard website
+
+response =  requests.get("https://www.billboard.com/charts/hot-100/2024-05-12/")
+
+soup = BeautifulSoup(response.text, "html.parser")
+
+musics_divs = soup.find_all(name = "div", class_ = "o-chart-results-list-row-container")
+
+musics = [item.select(selector = "div ul .lrv-u-width-100p ul li h3") for item in musics_divs]
+
+MUSICS = [music[0].string.strip() for music in musics]
+
+print(MUSICS[0])
+
+
+
+
